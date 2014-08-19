e04f514a93febcf007515b86724fbfc9a62775c8
Aymeric <aymeric@kodono.info>
2014-08-19 10:11:36 +0200

    First commit

---
+++ programs.js
@@ -0,0 +1,12 @@
+﻿exports.action = function(data, callback, config, SARAH) {
+  var exec = require('child_process').exec;
+  if (data.progValue == "xbmc") {
+    var process = '"C:\\Program Files (x86)\\XBMC\\XBMC.exe"';
+    console.log(process);
+    var child = exec(process, function (error, stdout, stderr) {
+      if (error !== null) console.log('exec error: ' + error);
+    });
+    
+    callback({'tts': "Je lance XBMC."});
+  }
+}
